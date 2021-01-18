da5fd1848a2f7fa756b5a7e187a634864b126aa6
yuanms2 <yuanms2@lenovo.com>
2021-01-18 16:07:17 +0800

    style:动态设置画布内部大小、css样式控制画布

--- src/pages/Three/index.js
+++ src/pages/Three/index.js
@@ -1,26 +1,41 @@
 import React, { useEffect } from 'react';
 import * as THREE from 'three';
-// import './index.css';
+import './index.css';
 
 const Three = () => {
+  const resizeRendererToDisplaySize = (renderer) => {
+    const canvas = renderer.domElement;
+    // const width = canvas.clientWidth;
+    // const height = canvas.clientHeight;
 
-  // const makeInstance = (geometry, color, x) => {
-  //   const material = new THREE.MeshPhongMaterial({color});
-
-  //   const cube = new THREE.Mesh(geometry, material);
-  //   scene.add(cube);
-
-  //   cube.position.x = x;
-
-  //   return cube;
-  // }
+    // 处理HD-DPI（ high-density dot per inch displays）
+    const pixelRatio = window.devicePixelRatio;
+    const width  = canvas.clientWidth  * pixelRatio | 0;
+    const height = canvas.clientHeight * pixelRatio | 0;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+    }
+    return needResize;
+  }
 
   useEffect(() => {
     const main = () => {
+
+      const makeInstance = (geometry, color, x) => {
+        const material = new THREE.MeshPhongMaterial({color});
+        const cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
+        cube.position.x = x;
+
+        return cube;
+      }
+
       // c创建场景
       const canvas = document.querySelector('#c');
       const renderer = new THREE.WebGLRenderer({canvas});
-      renderer.setSize(window.innerWidth, window.innerHeight);
+      // 设置 画布内部尺寸
+      // renderer.setSize(window.innerWidth, window.innerHeight);
 
       const fov = 75;
       const aspect = 2;  // the canvas default
@@ -38,20 +53,44 @@ const Three = () => {
       // 创建材料 并加入到场景中
       // MeshPhongMaterial 会被光源影响   MeshBasicMaterial不会被光源影响
       const material = new THREE.MeshPhongMaterial({color: 0x44aa88});
-      const cube = new THREE.Mesh(geometry, material);
-      scene.add(cube);
-      renderer.render(scene, camera);
+      // 创建对象（Mesh)
+      // const cube = new THREE.Mesh(geometry, material);
+      // scene.add(cube);
+      // renderer.render(scene, camera);
+
+      // 基础体：cube
+      const cubes = [
+        makeInstance(geometry, 0x44aa88,  0),
+        makeInstance(geometry, 0x8844aa, -2),
+        makeInstance(geometry, 0xaa8844,  2),
+      ];
+
       // 旋转
       function render(time) {
         time *= 0.001;  // convert time to seconds
 
-        cube.rotation.x = time;
-        cube.rotation.y = time;
+        // cube.rotation.x = time;
+        // cube.rotation.y = time;
+
+        cubes.forEach((cube, ndx) => {
+          const speed = 1 + ndx * .1;
+          const rot = time * speed;
+          cube.rotation.x = rot;
+          cube.rotation.y = rot;
+        });
+        // 动态设置 画布内部size
+        if (resizeRendererToDisplaySize(renderer)) {
+          const canvas = renderer.domElement;
+          camera.aspect = canvas.clientWidth / canvas.clientHeight;
+          console.log( canvas.clientWidth , canvas.clientHeight)
+          camera.updateProjectionMatrix();
+        }
 
         renderer.render(scene, camera);
 
         requestAnimationFrame(render);
       }
+
       requestAnimationFrame(render);
 
       const color = 0xFFFFFF;
@@ -67,8 +106,19 @@ const Three = () => {
 
   }, [])
 
-  return (<div>
-    <canvas id='c'></canvas>
+  return (<div className='page'>
+    <p>
+      sghowngowhgwleghw wehogweognwgj woghowego Nam augue nisi, elementum id diam vel, blandit imperdiet
+      nunc.  Vivamus facilisis imperdiet neque id porttitor.  Mauris sapien
+      felis, mollis tempus orci vitae, sollicitudin varius augue.  Nullam non
+      magna id sem faucibus sollicitudin.  Proin nunc mi, rutrum et elementum
+      ut, auctor eget massa. <canvas className='canvas' id='c'></canvas> Nam augue nisi, elementum id diam vel,
+      blandit imperdiet nunc.  Vivamus facilisis imperdiet neque id porttitor.  Mauris sapien
+      felis, mollis tempus orci vitae, sollicitudin varius augue.  Nullam non
+      magna id sem faucibus sollicitudin.  Proin nunc mi, rutrum et elementum
+      ut, auctor eget massa.
+    </p>
+
   </div>);
 }
 
